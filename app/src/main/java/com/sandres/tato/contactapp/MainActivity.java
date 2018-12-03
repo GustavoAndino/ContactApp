bf0f50bfc1c15930c21ef49e2d085862c19a8d04
Gustavo Andino <ces1411@hotmail.com>
2018-12-03 12:36:59 -0600

    Initial commit

---
+++ app/src/main/java/com/sandres/tato/contactapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.sandres.tato.contactapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lvContactos;
+    ArrayList<Contactos> contactos;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvContactos=(ListView) findViewById(R.id.lvContactos);
+        contactos = new ArrayList<>();
+        contactos.add(new Contactos("Carlos", "Alvarez",22232425,"Quezada"));
+        contactos.add(new Contactos("Marcelo", "Alvarez",22232426,"Quezada1"));
+        contactos.add(new Contactos("Julio", "Andino",22232427,"Quezada2"));
+        contactos.add(new Contactos("Natalie", "Coello",22232428,"Quezada3"));
+        contactos.add(new Contactos("Julissa", "Zuniga",22232429,"Quezada4"));
+
+        ArrayList<String> nombreContactos = new ArrayList<>();
+
+        for (Contactos contacto: contactos){
+            nombreContactos.add(contacto.getNombre+contacto.getAppellido());
+        }
+
+
+    }
+}
